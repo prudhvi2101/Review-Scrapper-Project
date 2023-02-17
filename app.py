7113295bf2346aab5d6328da5a7d06bdcc1a0165
prudhvi2101 <124028934+prudhvi2101@users.noreply.github.com>
2023-02-17 20:51:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,18 +17,16 @@ def hello_world():
 def show():
     if (request.method == 'POST'):
         try:
-            searchstring = request.form['products'].replace(" ","")
+            searchstring = request.form['content'].replace(" ","")
             domain = 'https://www.amazon.in'
-            web_url = 'https://www.amazon.in/s?k='+ searchstring
-            amazon = uReq(web_url)
-            amazon_page = amazon.read()
-            amazon.close()
-            amazon_html = bs(amazon_page, 'html.parser')
-            print(amazon_html)
+            web_url = f'https://www.amazon.in/s?k={searchstring}'
+            headers = {"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"}
+            page = requests.get(url=web_url, headers=headers)
+            amazon_html = bs(page.content, 'html.parser')
+            print(amazon_html.show_status)
             bigboxes = amazon_html.find_all('div', {'class': 'sg-col-20-of-24 s-result-item s-asin sg-col-0-of-12 sg-col-16-of-20 sg-col s-widget-spacing-small sg-col-12-of-16'})
-            box1 = bigboxes[1]
+            box1 = bigboxes[2]
             product1 = domain+box1.div.div.div.div.div.div.div.span.a['href']
-            print(product1)
             product1_html = bs(uReq(product1).read(), 'html.parser')
             reviews = product1_html.find_all("div", {'class':'a-section review aok-relative'})
 
@@ -48,7 +46,7 @@ def show():
                 mydict = {"Product": searchstring, "Rating": Rating, "Comment": Comment}
                 Product_Ratings.append(mydict)
 
-            return render_template('result.html', Product_Ratings=Product_Ratings[0:(len(reviews)-1)])
+            return render_template('result.html', Product_Ratings=Product_Ratings)
         except:
             return 'something is wrong'
 
@@ -58,4 +56,4 @@ def show():
 
 
 if __name__== "__main__":
-    app.run(host='0.0.0.0', port = 5000 )
+    app.run(host='0.0.0.0', port = 8000 )
