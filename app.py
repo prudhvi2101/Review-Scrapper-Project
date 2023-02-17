1089223cc623ddd26e4dc5b7076d8ecd26814ea1
prudhvi2101 <124028934+prudhvi2101@users.noreply.github.com>
2023-02-17 19:36:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS, cross_origin
-from requests import *
+import requests
 from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen as uReq
 
-app = Flask(__name__)
+application = Flask(__name__)
+app = application
 
 @app.route("/home", methods = ['GET'])
 @cross_origin()
@@ -19,7 +20,10 @@ def show():
             searchstring = request.form['products'].replace(" ","")
             domain = 'https://www.amazon.in'
             web_url = 'https://www.amazon.in/s?k='+ searchstring
-            amazon_html = bs(uReq(web_url).read(), 'html.parser')
+            amazon = uReq(web_url)
+            amazon_page = amazon.read()
+            amazon.close()
+            amazon_html = bs(amazon_page, 'html.parser')
             print(amazon_html)
             bigboxes = amazon_html.find_all('div', {'class': 'sg-col-20-of-24 s-result-item s-asin sg-col-0-of-12 sg-col-16-of-20 sg-col s-widget-spacing-small sg-col-12-of-16'})
             box1 = bigboxes[1]
